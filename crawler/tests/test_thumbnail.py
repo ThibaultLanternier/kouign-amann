c9d26e3ae5a9a855a24f591161ea14d928959eb6
Thibault Lanternier <tlanternier@meilleursagents.com>
2023-01-24 14:54:31 +0100

    feat: add async Thumbnail generator

---
+++ crawler/tests/test_thumbnail.py
@@ -0,0 +1,58 @@
+import unittest
+from pathlib import Path
+
+from PIL import Image
+
+from app.controllers.thumbnail import ThumbnailImage
+from app.models.picture import PictureOrientation
+
+CANON_EOS_70D_THUMBNAIL = "/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAADAAUDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwCW+sbTUPKkvbaK5cAgGZQ+BnHGenTtRRRWFVtTZ0UkuRH/2Q=="  # noqa: E501
+VERTICAL_IMAGE_THUMBNAIL = "/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAAKAAcDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwDGh1G30fQ7W8vNhFwCY4UbMjnPX0A6/pRXn92Sd3J4VQPbpRXYsRNKyMPZRep//9k="  # noqa: E501
+HORIZONTAL_IMAGE_THUMBNAIL = "/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAAHAAoDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwDSvLy51O1ktm1AzQW8wXURdF2IiI58sjoevSuGbwJ4clcyRXWreWx3JxH0PTrRRU124zaRdBJ002f/2Q=="  # noqa: E501
+
+CANON_EOS_70D_IMAGE_NAME = "tests/files/test-canon-eos70D.jpg"
+VERTICAL_IMAGE_NAME = "tests/files/test_image.JPG"
+HORIZONTAL_IMAGE_NAME = "tests/files/IMG_7095-300px.JPG"
+
+
+class TestThumbnailImage(unittest.IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        self.canon_eos_70_picture = Image.open(Path(CANON_EOS_70D_IMAGE_NAME))
+        self.vertical_image = Image.open(Path(VERTICAL_IMAGE_NAME))
+        self.horizontal_image = Image.open(Path(HORIZONTAL_IMAGE_NAME))
+
+    async def test_get_base64_thumbnail(self):
+        image = ThumbnailImage(self.canon_eos_70_picture, thumbnail_size=5)
+        base64_thumbnail = await image.get_base64_thumbnail()
+
+        self.assertEqual(CANON_EOS_70D_THUMBNAIL, base64_thumbnail)
+
+    async def test_get_base64_thumbnail_vertical(self):
+        image = ThumbnailImage(self.vertical_image, thumbnail_size=10)
+        base64_thumbnail = await image.get_base64_thumbnail()
+
+        self.assertEqual(VERTICAL_IMAGE_THUMBNAIL, base64_thumbnail)
+
+    async def test_get_base64_thumbnail_horizontal(self):
+        image = ThumbnailImage(self.horizontal_image, thumbnail_size=10)
+        base64_thumbnail = await image.get_base64_thumbnail()
+
+        self.assertEqual(HORIZONTAL_IMAGE_THUMBNAIL, base64_thumbnail)
+
+    async def test_get_base64_original_image_unchanged(self):
+        start_size = self.vertical_image.size
+
+        image = ThumbnailImage(self.vertical_image)
+        await image.get_base64_thumbnail()
+
+        self.assertEqual(start_size, self.vertical_image.size)
+
+    async def test_get_orientation_vertical(self):
+        image = ThumbnailImage(self.vertical_image)
+
+        self.assertEqual(PictureOrientation.PORTRAIT, await image.get_orientation())
+
+    async def test_get_orientation_horizontal(self):
+        image = ThumbnailImage(self.horizontal_image, thumbnail_size=5)
+
+        self.assertEqual(PictureOrientation.LANDSCAPE, await image.get_orientation())
